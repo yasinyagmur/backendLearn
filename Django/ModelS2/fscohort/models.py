670620d3742d6f7c82fa73368b877b653a22566e
yasin yağmur <yasin.yagmur@gmail.com>
2022-12-21 20:47:13 +0300

    fixed

--- Django/ModelS2/fscohort/models.py
+++ Django/ModelS2/fscohort/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=25,blank=True)
+    last_name = models.CharField(max_length=25)
+    number = models.PositiveSmallIntegerField(blank=True)
