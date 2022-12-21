852b2ddfa45eb6f1da414ea36c3a244539f13d26
yasin yağmur <yasin.yagmur@gmail.com>
2022-12-21 21:00:35 +0300

    v2

--- Django/ModelS2/fscohort/models.py
+++ Django/ModelS2/fscohort/models.py
@@ -6,4 +6,11 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=25,blank=True)
     last_name = models.CharField(max_length=25)
-    number = models.PositiveSmallIntegerField(blank=True)
+    number = models.PositiveSmallIntegerField(blank=True,null=True)
+    def __str__(self) -> str:
+        return f"{self.number}- {self.first_name}"
+    
+    class Meta:
+        ordering = ["number"]
+        verbose_name_plural = "Students List"
+        # db_table = "" tablo ismini değiştirir
