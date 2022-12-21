c826b55bd907062ae1047ee08a3e331fbdfda21f
yasin yağmur <yasin.yagmur@gmail.com>
2022-12-21 21:47:30 +0300

    v3

--- Django/ModelS2/fscohort/models.py
+++ Django/ModelS2/fscohort/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=25,blank=True)
-    last_name = models.CharField(max_length=25)
-    number = models.PositiveSmallIntegerField(blank=True,null=True)
-    def __str__(self) -> str:
-        return f"{self.number}- {self.first_name}"
-    
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    number = models.PositiveSmallIntegerField(blank=True, null=True)
+    about = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    is_active = models.BooleanField(default=True)
+    avatar = models.ImageField(blank=True, null=True, upload_to='student')
+    files = models.FileField(blank=True, null=True, upload_to='student_files')
+    register_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.number} - {self.first_name} {self.last_name}'
+
     class Meta:
-        ordering = ["number"]
-        verbose_name_plural = "Students List"
-        # db_table = "" tablo ismini değiştirir
+        ordering = ('-number',)
+        verbose_name = 'Öğrenci'
+        verbose_name_plural = 'Öğrenciler'
+        # db_table = ''  tablo ismini değiştir
