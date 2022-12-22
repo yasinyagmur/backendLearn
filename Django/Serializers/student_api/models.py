ae1453aa0c839383d7c268035508dbc260f575bb
yasin yağmur <yasin.yagmur@gmail.com>
2022-12-22 20:57:06 +0300

    model created

--- Django/Serializers/student_api/models.py
+++ Django/Serializers/student_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    firt_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    number = models.IntegerField(blank=True,null=True)
+    age = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.firt_name}"
