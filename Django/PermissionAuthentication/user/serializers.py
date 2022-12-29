bfc6e7fa3453bb21ea9b24c15803a8d4a69a4d9b
yasin yağmur <yasin.yagmur@gmail.com>
2022-12-29 21:59:25 +0300

    lesson 2

--- Django/PermissionAuthentication/user/serializers.py
+++ Django/PermissionAuthentication/user/serializers.py
@@ -1,2 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required = True, validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(write_only = True)
+    password2 = serializers.CharField(write_only = True,required = True)
+    first_name = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+            'password2'
+        )
