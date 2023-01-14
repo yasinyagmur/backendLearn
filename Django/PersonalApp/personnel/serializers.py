a32a90606f82955aadda9a687b38a1e3aa2e3392
yasin yağmur <yasin.yagmur@gmail.com>
2023-01-14 13:48:16 +0300

    permission crate put delete

--- Django/PersonalApp/personnel/serializers.py
+++ Django/PersonalApp/personnel/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Department,Personnel
-
+from django.utils.timezone import now
 
 class DepartmentSerializer(serializers.ModelSerializer):
     
@@ -15,7 +15,20 @@ class DepartmentSerializer(serializers.ModelSerializer):
         return obj.personals.count()
 
 class PersonnelSerializer(serializers.ModelSerializer):
-
+    
+    days_since_joined = serializers.SerializerMethodField()
+    create_user_id = serializers.IntegerField(required = False)
+    create_user = serializers.StringRelatedField()
+    
     class Meta:
         model = Personnel
-        fields = '__all__'
+        fields = "__all__"
+        
+        
+    # def create(self, validated_data):
+    #     validated_data["create_user_id"] = self.context['request'].user.id
+    #     instance = Personnel.objects.create(**validated_data)
+    #     return instance
+        
+    def get_days_since_joined(self, obj):
+        return (now() - obj.start_date).days
--- Django/PersonalApp/personnel/urls.py
+++ Django/PersonalApp/personnel/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DepartmentView,PersonnelListCreateView
+from .views import DepartmentView,PersonnelListCreateView,PersonalGetUpdateDelete
 
 
 urlpatterns = [
     path('department/', DepartmentView.as_view()),
     path('personnel/', PersonnelListCreateView.as_view()),
+    path('personnel/<int:pk>', PersonalGetUpdateDelete.as_view()),
 ]
