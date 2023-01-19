55537d59a2cfee6d8517a5a16edbca314d18e9c1
yasin yağmur <yasin.yagmur@gmail.com>
2023-01-19 21:01:13 +0300

    model admin

--- Django/Admin_site_custom/products/admin.py
+++ Django/Admin_site_custom/products/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_date", "is_in_stock", "update_date")
+    list_editable = ( "is_in_stock",)
+    # list_display_links = ("create_date", )
+    list_filter = ("is_in_stock", "create_date")
+
+
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.site_title = "Clarusway Title"
+admin.site.site_header = "Clarusway Admin Portal"  
+admin.site.index_title = "Welcome to Clarusway Admin Portal"
